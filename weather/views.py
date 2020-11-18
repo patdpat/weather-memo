1016a170b777c9f346e6c106909a9001dc9e96fd
sidtipat.k <s.kietchai@outlook.com>
2020-11-19 00:13:57 +0700

    add new logicc

--- weather/views.py
+++ weather/views.py
@@ -4,13 +4,28 @@ from .models import WH
 from django.utils import timezone
 from django.views.decorators.csrf import csrf_exempt
 
+
 def create_url(w_text):
     head = 'https://api.waqi.info/feed/'
     tail = '/?token=d66c4f6d02ad82aee0b65500bb015164280ac4a8'
-    url = head + w_text+ tail
+    url = head + w_text + tail
     return url
 
+
+def should_go_out(city_weather):
+    descrip = city_weather['description']
+    pm = city_weather['pm']
+    if 'rain' in descrip:
+        return False
+    else:
+        if float(pm) > 50:
+            return False
+        else:
+            return True
+
 # Create your views here.
+
+
 def list_w(request):
     weather = WH.objects.all().order_by('-added_date')
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=c812a2a33fd7892739ba5b4c09b2e499'
@@ -18,21 +33,23 @@ def list_w(request):
     for w in weather:
         r = requests.get(url.format(w.text)).json()
         city_weather = {'temperature': r['main']['temp'],
-                    'description': r['weather'][0]['description'],
-                    'icon': r['weather'][0]['icon'],
-                    }
+                        'description': r['weather'][0]['description'],
+                        'icon': r['weather'][0]['icon'],
+                        }
         r2 = requests.get(create_url(w.text)).json()
-        city_weather['pm']= r2['data']['aqi']
-        #append result part
-        if float(city_weather['temperature']) < 13 or float(city_weather['temperature'])>31:
-            city_weather['result']='Today is not safe for outdoor'
+        city_weather['pm'] = r2['data']['aqi']
+        # append result part
+        result = should_go_out(city_weather)
+        if result == False:
+            city_weather['result'] = 'Weather is not suit for outdoor activity'
         else:
-            city_weather['result']='Today is safe for outdoor'
-            
-        
-        WH.objects.filter(text=w.text).update(temperature=city_weather['temperature'], descrip=city_weather['description'], icon=city_weather['icon'], result=city_weather['result'],pm=city_weather['pm'])
+            city_weather['result'] = 'Weather is suit for outdoor activity'
+
+        WH.objects.filter(text=w.text).update(temperature=city_weather['temperature'], descrip=city_weather[
+            'description'], icon=city_weather['icon'], result=city_weather['result'], pm=city_weather['pm'])
 
     return render(request, 'weather/index.html', {'weather': weather})
+
 
 @csrf_exempt
 def add_city(request):
@@ -42,24 +59,27 @@ def add_city(request):
 
     r = requests.get(url.format(city)).json()
 
-    city_weather= {'city': city,
-            'temperature': r['main']['temp'],
-            'description' : r['weather'][0]['description'],
-            'icon' : r['weather'][0]['icon'],
-    
-                  }
+    city_weather = {'city': city,
+                    'temperature': r['main']['temp'],
+                    'description': r['weather'][0]['description'],
+                    'icon': r['weather'][0]['icon'],
+
+                    }
     r2 = requests.get(create_url(city)).json()
-    city_weather['pm']= r2['data']['aqi']
-    #append result part
-    if float(city_weather['temperature']) < 13 or float(city_weather['temperature'])>31:
-            city_weather['result']='Today is not safe for outdoor'
+    city_weather['pm'] = r2['data']['aqi']
+
+    result = should_go_out(city_weather)
+    if result == False:
+        city_weather['result'] = 'Weather is not suit for outdoor activity'
     else:
-        city_weather['result']='Today is safe for outdoor'
-    WH.objects.create(added_date=current_date, text=city_weather['city'], temperature=city_weather['temperature'], descrip=city_weather['description'], icon=city_weather['icon'], result=city_weather['result'],pm=city_weather['pm'])
-   
+        city_weather['result'] = 'Weather is suit for outdoor activity'
+    WH.objects.create(added_date=current_date, text=city_weather['city'], temperature=city_weather['temperature'],
+                      descrip=city_weather['description'], icon=city_weather['icon'], result=city_weather['result'], pm=city_weather['pm'])
+
     return redirect('list_w')
 
+
 @csrf_exempt
-def delete_city(request,pk):
+def delete_city(request, pk):
     WH.objects.filter(id=pk).delete()
     return redirect('list_w')
